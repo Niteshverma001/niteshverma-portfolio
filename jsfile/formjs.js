f067be0491e4bc08b8b091b46374900a495b3c7a
Nitesh Verma <129258309+Niteshverma001@users.noreply.github.com>
2023-06-22 10:13:13 +0530

    Update formjs.js

--- jsfile/formjs.js
+++ jsfile/formjs.js
@@ -16,4 +16,18 @@ document.getElementById("hireForm").addEventListener("submit", function(event) {
     // Reset form fields
     document.getElementById("hireForm").reset();
   });
-  
+
+
+function sendemail(){
+    Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
